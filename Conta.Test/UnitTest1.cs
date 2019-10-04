c6e4fbfde9709015844042788e2937680f5f05be
Antonio A C Cardozo <gutorlz@gmail.com>
2019-10-04 00:10:03 -0300

    MsTest Added

---
+++ Conta.Test/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Principal;
+
+namespace ContaTest.MSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Conta conta;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+           conta = new Conta("0009", 200);
+        }
+
+        [Ignore]
+        [TestCategory("Sacar Valores")]
+        [TestMethod]
+        public void PermiteSacarValor()
+        {
+            bool resultado = conta.Sacar(150);
+
+            Assert.IsTrue(resultado);
+        }
+
+        [TestMethod]
+        public void NaoPermiteSacarValorSemSaldo()
+        {
+            bool resultado = conta.Sacar(250);
+
+            Assert.IsFalse(resultado);
+        }
+
+        [TestMethod]
+        public void NaoPermiteSacarValorNegativo()
+        {
+            bool resultado = conta.Sacar(-250);
+
+            Assert.IsFalse(resultado);
+        }
+    }
+}
